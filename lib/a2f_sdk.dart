715e26d1f9fee04efa2a9e9779b41c2beac16fa0
Gianmarco Calbi <calbigianmarco@gmail.com>
2024-06-29 15:22:08 +0200

    ref: consider to remove isar from the SDK
    Closes #10

--- lib/a2f_sdk.dart
+++ lib/a2f_sdk.dart
@@ -3,14 +3,11 @@ library a2f_sdk;
 
 export 'package:equatable/equatable.dart';
 export 'package:flext/flext.dart';
-export 'package:isar/isar.dart';
-export 'package:isar_flutter_libs/isar_flutter_libs.dart';
 export 'package:path_provider/path_provider.dart';
 export 'package:the_umpteenth_logger/the_umpteenth_logger.dart';
 
 export 'src/config/config.dart';
 export 'src/core/core.dart';
-export 'src/db/db.dart';
 export 'src/extensions/extensions.dart';
 export 'src/framework/framework.dart';
 export 'src/mixin/mixin.dart';
