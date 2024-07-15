74ade34f63f2e46f0417a70e830fd268d023e72f
Gianmarco Calbi <calbigianmarco@gmail.com>
2024-07-15 17:55:19 +0200

    feat: add NoTransitionsBuilder
    Closes #20

--- lib/a2f_sdk.dart
+++ lib/a2f_sdk.dart
@@ -3,6 +3,7 @@ library a2f_sdk;
 
 export 'package:equatable/equatable.dart';
 export 'package:flext/flext.dart';
+export 'package:path/path.dart';
 export 'package:path_provider/path_provider.dart';
 export 'package:the_umpteenth_logger/the_umpteenth_logger.dart';
 
@@ -12,5 +13,6 @@ export 'src/extensions/extensions.dart';
 export 'src/framework/framework.dart';
 export 'src/models/models.dart';
 export 'src/style/style.dart';
+export 'src/theme/theme.dart';
 export 'src/utils/utils.dart';
 export 'src/widgets/widgets.dart';
